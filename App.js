349ddfea509bcf19cc6c1b42986e60256d3242b3
IvanUpdate <mironovvan99@gmail.com>
2022-05-14 18:35:46 +0300

    completed design for main view

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native';
+import {colors} from './Colors';
+import {AntDesign} from '@expo/vector-icons';
+import {tempData} from "./tempData";
+import TodoList from "./components/TodoList";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.divider}/>
+                <Text style={styles.left_title}>Todo </Text>
+                <Text style={styles.right_title}>Lists</Text>
+                <View style={styles.divider}/>
+            </View>
+            <View style={styles.main}>
+                <TouchableOpacity style={styles.addList}>
+                    <AntDesign name="plus" size={16} color={colors.blue}/>
+                </TouchableOpacity>
+                <Text style={styles.addLabel}>Add List</Text>
+            </View>
+            <View style={{height: 275, paddingLeft: 32}}>
+                <FlatList
+                    data={tempData}
+                    keyExtractor={item => item.name}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={({item}) => <TodoList list = {item} />}
+                />
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: colors.white,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        flexDirection: "row",
+    },
+    divider: {
+        backgroundColor: colors.lightblue,
+        height: 1,
+        flex: 1,
+        alignSelf: "center",
+    },
+    right_title: {
+        fontSize: 38,
+        fontWeight: "300",
+        color: colors.blue,
+        paddingRight: 64,
+    },
+    left_title: {
+        fontSize: 38,
+        fontWeight: "800",
+        color: colors.black,
+        paddingLeft: 64,
+    },
+    main: {
+        marginVertical: 48,
+    },
+    addList: {
+        borderWidth: 2,
+        borderColor: colors.lightblue,
+        borderRadius: 4,
+        padding: 16,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    addLabel: {
+        color: colors.blue,
+        fontWeight: "600",
+        fontSize: 14,
+        marginTop: 8
+    }
 });
---
+++ tempData.js
@@ -0,0 +1,66 @@
+//temporary data until we connect ot FireBase database
+
+export const tempData = [
+    {
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport check",
+                completed: true
+            },
+            {
+                title: "Reserve hotel room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            },
+        ]
+    },
+    {
+        name: "Выучить React",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Выучить JS",
+                completed: false
+            },
+            {
+                title: "Написать проект",
+                completed: true
+            },
+            {
+                title: "Выучить React Native",
+                completed: true
+            },
+        ]
+    },
+    {
+        name: "Переехать пожить в другую страну",
+        color: "#8c0c8a",
+        todos: [
+            {
+                title: "Выбрать страну",
+                completed: false
+            },
+            {
+                title: "Найти удаленную работу",
+                completed: false
+            },
+            {
+                title: "Скопить деньги",
+                completed: false
+            },
+            {
+                title: "Купить билеты",
+                completed: false
+            },
+        ]
+    },
+]
